739e4af5cc3aa6cdabba8bce546d4c6fcb9b0a98
Hansen <s3443714@student.rmit.edu.au>
2017-03-10 18:24:19 +1100

    Testing from Hansen

--- pacOnline/resources/views/pages/home.blade.php
+++ pacOnline/resources/views/pages/home.blade.php
@@ -31,7 +31,7 @@
 <div class="jumbotron">
   <div class="container text-center">
     <h1>Pac Online</h1>      
-    <p>Mission, Vission & Values</p>
+    <p>Mission, Hansen & Values</p>
   </div>
 </div>
 
