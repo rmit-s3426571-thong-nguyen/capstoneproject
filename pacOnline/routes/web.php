93ad3a5cd86b5ca005b18232dda447f7c73d58fe
Hansen <s3443714@student.rmit.edu.au>
2017-03-10 18:27:10 +1100

    changed web.php

--- pacOnline/routes/web.php
+++ pacOnline/routes/web.php
@@ -12,5 +12,35 @@
 */
 
 Route::get('/', function () {
-    return view('pages/home');
+	return realpath(base_path('resources/views'));
+    return view('welcome');
 });
+Route::get('users', ['uses' => 'UsersController@index']);
+Route::get('users/create', ['uses' => 'UsersController@create']);
+Route::post('users', ['uses' => 'UsersController@store']);
+
+/*
+Route::get('users', function() {
+	$users = [
+		'0' => [
+			'first_name' => 'Hansen',
+			'last_name' => 'Phanggestu',
+			'location' => 'Melbourne'
+		],
+		'1' => [
+			'first_name' => 'Hansenn',
+			'last_name' => 'Phanggestuuu',
+			'location' => 'Sydney'
+		]
+
+	];
+	return $users;
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
