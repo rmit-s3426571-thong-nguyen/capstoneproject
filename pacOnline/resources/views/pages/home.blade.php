01ca0dba1c0a658c2cce47cb2cdbb5e20f1140d8
James Yap <s3493001@student.rmit.edu.au>
2017-03-10 18:19:19 +1100

    Testing from James

--- pacOnline/resources/views/pages/home.blade.php
+++ pacOnline/resources/views/pages/home.blade.php
@@ -31,7 +31,7 @@
 <div class="jumbotron">
   <div class="container text-center">
     <h1>Pac Online</h1>      
-    <p>Mission, Vission & Values</p>
+    <p>James, Vission & Values</p>
   </div>
 </div>
 
