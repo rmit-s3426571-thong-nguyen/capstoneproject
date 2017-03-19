f3b056c07a0deba9b7329cf9880a31b4317fbaf6
Thong Nguyen <s3426571@student.rmit.edu.au>
2017-03-19 14:20:01 +1100

    adding products and view products

--- pacOnline/app/Http/Controllers/ProductController.php
+++ pacOnline/app/Http/Controllers/ProductController.php
@@ -8,10 +8,47 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     // get all products from database and pass to index to render.
-    public function getIndex()
+    public function index()
     {
-        $product = Product::latest()->get();
+    	//using Elequent to get all the products
+        $products = Product::latest()->get();
 
-        return view ('shop.index',['products' => $product]);
+        return view('shop.index',compact('products'));
+    }
+
+
+    public function create()
+    {
+    	return view('shop.create');
+    }
+
+    public function show(Product $product)
+    {
+    	return view('shop.show', compact('product'));
+    }
+
+    public function store()
+    {
+        //form validation
+        $this->validate(request(),[
+            'title' => 'required',
+            'desc' => 'required',
+            'price' => 'required',
+            'imageLocation' => 'required',
+        ]);
+
+        // create a new product
+        $product = new Product;
+
+        $product->title = request('title');
+        $product->desc = request('desc');
+        $product->price = request('price');
+        $product->imageLocation = request('imageLocation');
+
+        // save to the database
+        $product->save();
+
+        // redirect to home page
+        return redirect('/');
     }
 }
