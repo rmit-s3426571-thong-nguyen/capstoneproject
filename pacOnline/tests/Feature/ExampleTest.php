b57917ee15cc2cbff584805e61c5715f40a212e9
James Yap <s3493001@student.rmit.edu.au>
2017-03-22 14:39:27 +1100

    Created unit tests for user login

--- pacOnline/tests/Feature/ExampleTest.php
+++ pacOnline/tests/Feature/ExampleTest.php
@@ -14,10 +14,53 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    public function testNewUserRegistration()
+    {
+        $this->visit('/register');
+        $this->type('Hansenn13', 'name');
+        $this->type('Hansenn13@gmail.com', 'email');
+        $this->type('123456', 'password');
+        $this->type('123456', 'password_confirmation');
+        $this->press('Register');
+        $this->seePageIs('/');
     }
+
+    public function testEmailDatabase()
+    {
+
+        $this->seeInDatabase('users', ['email' => 'Hansen@gmail.com']);
+    }
+
+    public function testNameDatabase()
+    {
+
+        $this->seeInDatabase('users', ['name' => 'Hansen']);
+    }
+
+    public function testUserLogin()
+    {
+        $this->visit('/login');
+        $this->type('Hansenn@gmail.com', 'email');
+        $this->type('123456', 'password');
+        $this->press('Login');
+        $this->seePageIs('/');
+    }
+
+      public function testUserLogin2()
+    {
+        $this->visit('/login');
+        $this->type('Hansenn2@gmail.com', 'email');
+        $this->type('123456', 'password');
+        $this->press('Login');
+        $this->seePageIs('/');
+    }
+
+    public function forgotPassword()
+    {
+        $this->visit('/');
+        $this->click('About US');
+        $this->seePageIs('/about');
+    }
+   
 }
