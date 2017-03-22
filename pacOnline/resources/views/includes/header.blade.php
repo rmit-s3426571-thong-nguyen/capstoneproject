35340f20b3762cc3a103244192c9acb5a6c2a703
Thong Nguyen <s3426571@student.rmit.edu.au>
2017-03-22 12:54:24 +0000

    fix routing

--- pacOnline/resources/views/includes/header.blade.php
+++ pacOnline/resources/views/includes/header.blade.php
@@ -26,7 +26,7 @@
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 @if (Auth::guest())
-                    <li><a href="http://localhost:8000/login"><span class="glyphicon glyphicon-user"></span> Login / Register </a></li>
+                    <li><a href=" {{ route('login') }}" ><span class="glyphicon glyphicon-user"></span> Login / Register </a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
