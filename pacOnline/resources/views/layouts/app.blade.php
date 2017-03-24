0f8ab0a0340d7e711f2d63e97062e6b89180cd94
Diem Lam <s3484263@student.rmit.edu.au>
2017-03-24 18:04:17 +1100

    Changed Laravel tp PAC Online on login page

--- pacOnline/resources/views/layouts/app.blade.php
+++ pacOnline/resources/views/layouts/app.blade.php
@@ -8,14 +8,14 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'PAC Online') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
-        window.Laravel = {!! json_encode([
+        window.PAC Online = {!! json_encode([
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
@@ -36,7 +36,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'PAC Online') }}
                     </a>
                 </div>
 
