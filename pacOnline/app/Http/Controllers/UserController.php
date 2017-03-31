501396d9daf80b33ea4f99d8eaeaa43fee9b81c2
Hansen <s3443714@student.rmit.edu.au>
2017-03-31 15:51:13 +1100

    edited my details page

--- pacOnline/app/Http/Controllers/UserController.php
+++ pacOnline/app/Http/Controllers/UserController.php
@@ -6,19 +6,21 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function save(Request $request)
+    public function save($id, mydetails $request)
     {
         // Stores new user record in database and assigns role
         $role_user = App\Role::where('name', 'user')->first();
 
         $user = new App\User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->birth = $request->birth;
-        $user->address = $request->address;
-        $user->password = $request->password;
+        $user->user_id = auth()->id();
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->birth = $request->get('birth');
+        $user->address = $request->get('address');
+        $user->password = $request->get('password');
         $user->save();
         $user->roles()->attach($role_user);
+        return \Redirect::route('users.mydetails', [$user->id])->with('message', 'User has been updated!');
     }
 
     public function displaydetails($id)
--- pacOnline/resources/views/User/mydetails.blade.php
+++ pacOnline/resources/views/User/mydetails.blade.php
@@ -12,6 +12,21 @@
 				<div class="panel panel-default">
 					<div class="panel-heading"> My Details </div>
 					
+					<form method="POST" action="">
+  						<input type="hidden" name="_token" value="">
+  						<input type="hidden" name="_method" value="PUT">
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" name="name" value="" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" name="email" value="" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">
+    <i class="fa fa-btn fa-sign-in"></i>Update
+  </button>
+</form>
 				</div>
 			</div>
         </div>
