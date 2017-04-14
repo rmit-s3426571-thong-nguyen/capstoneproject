fa0fe01042aca8929f8caf450846720d808396d4
Diem Lam <s3484263@student.rmit.edu.au>
2017-04-15 02:34:04 +1000

    different method of searching. still trying to work out how to return results within title AND description

--- pacOnline/app/Http/Controllers/SearchController.php
+++ pacOnline/app/Http/Controllers/SearchController.php
@@ -25,6 +25,7 @@ class SearchController extends Controller
         //return view('search.result');
     }
 
+    /* ORIGINAL LINDEAR SEARCH FUNCTION
     public function search(Resquest $request)
     {
     	if($request->ajax()) {
@@ -45,5 +46,82 @@ class SearchController extends Controller
     			return Response($output);
     		}
     	}
+    } */
+
+
+    /* The following code was adapted from http://idiallo.com/blog/php-mysql-search-algorithm
+     * For educational purposes only
+     */
+
+    // Separates query string into individual words and filter query
+    public function filter(Request $request) {
+        $query = trim(preg_replace("/(\s+)+/", " ", $request));
+        $words =  array();
+        $filter = array("a","and","in","is","it","on","or","the");
+        $count = 0;
+
+        foreach (explode(" ", $query) as $key => $value) {
+            if (in_array($key, $list)) {
+                continue;
+            }
+            $words[] = $key;
+            if ($count >= 10) {
+                break;
+            }
+        }
+        return $words;
+    }
+    
+    public static function escape($string){
+        $con = self::connect();
+        return $con->real_escape_string($string);
+    }
+
+    // Search with score
+    public function search(Resquest $request)
+    {
+        $query = trim($request);
+
+        // Scores
+        $scoreFullTitle = 10; // Exact match in title
+        $scoreTitleKey = 5; // Match title in part
+        $scoreFullDesc = 5; //Exact match in description
+        $scoreDescKey = 3; // Match description in part
+        $scoreCat = 1; // Match with Category
+        $score = 0; // starting score
+
+        $keywords = filter($query);
+        $products = DB::table('products');
+        $titleSQL = array();
+        $descSQL = array();
+        $catSQL = array();
+
+        // Exact match ; higher chances of appearing at top of result
+        if (count($keywords)) {
+            $titleSQL[] = "if (title LIKE '%''.$query.''%', {$scoreFullTitle}, 0)";
+            $descSQL[] = "if (desc LIKE '%''.$query.''%', {$scoreFullDesc}, 0)";
+        }
+
+        // Part match
+        foreach($keywords as $key) {
+            $titleSQL[] = "if (title LIKE '%''.DB::escape($key).''%',,{$scoreTitleKey},0)";
+            $sumSQL[] = "if (desc LIKE '%''.DB::escape($key).''%',,{$scoreDescKey},0)";
+
+
+
+            if($products) {
+                foreach($products as $key => $product) {
+                    $output.='<tr>'.
+                             '<td>'.$product->imageLocation.'</td>'.
+                             '<td>'.$product->title.'</td>'.
+                             '<td>'.$product->desc.'</td>'.
+                             '<td>'.$product->price.'</td>'.
+                             '<td>'.$product->user->name.'</td>'.
+                             '<td>'.$product->created_at->diffForHumans.'</td>'.
+                             '</tr>';
+                }
+            return Response($output);
+            }
+        }
     }
 }
--- pacOnline/resources/views/shop/create.blade.php
+++ pacOnline/resources/views/shop/create.blade.php
@@ -5,49 +5,59 @@
 @endsection
 
 @section('content')
+<div class="container">
+	<div class="col-sm-12">
+		<h1>Post a product</h1>
+		<hr>
+		<form method="POST" action="/products">
 
-<div class="col-sm-12">
-	<h1>Post a product</h1>
-	<hr>
-	<form method="POST" action="/products">
+			{{ csrf_field() }}
+            <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                <div class="form-group">
+                    <label for="title"> Product Title</label>
+                    <input type="text" class="form-control" id="title" value="{{ old('title') }}" name="title">
+                </div>
+            </div>
 
-		{{ csrf_field() }}
+            <div class="form-group{{ $errors->has('desc') ? ' has-error' : '' }}">
+                <div class="form-group">
+                    <label for="desc">Desciption</label>
+                    <textarea type="body" class="form-control" id="desc" value="{{ old('desc') }}" name="desc"></textarea>
+                </div>
+            </div>
 
-		<div class="form-group">
-			<label for="title"> Product Title</label>
-			<input type="text" class="form-control" id="title" value="{{ old('title') }}" name="title">
-		</div>
+            <div class="form-group{{ $errors->has('category_id') ? ' has-error' : '' }}">
+                <div class="form-group">
+                    <label for="category_id">Category</label>
+                    <select class="form-control" name="category_id">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}"> {{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
 
-		<div class="form-group">
-			<label for="desc">Description</label>
-			<textarea type="body" class="form-control" id="desc" value="{{ old('desc') }}" name="desc"></textarea>
-		</div>
+            <div class="form-group{{ $errors->has('price') ? ' has-error' : '' }}">
+                <div class="form-group">
+                    <label for="price">Price</label>
+                    <textarea type="body" class="form-control" id="price" value="{{ old('price') }}" name="price" ></textarea>
+                </div>
+            </div>
 
-		<div class="form-group">
-			<label for="category_id">Category</label>
-			<select class="form-control" name="category_id">
-				@foreach($categories as $category)
-					<option value="{{ $category->id }}"> {{ $category->name }}</option>
-				@endforeach
-			</select>
-		</div>
+            <div class="form-group{{ $errors->has('imageLocation') ? ' has-error' : '' }}">
+                <div class="form-group">
+                    <label for="image">Images</label>
+                    <textarea type="body" class="form-control" id="imageLocation"  value="{{ old('imageLocation') }}" placeholder="Just paste a product image url here..." name="imageLocation" ></textarea>
+                </div>
+            </div>
 
-		<div class="form-group">
-			<label for="price">Price</label>
-			<textarea type="body" class="form-control" id="price" value="{{ old('price') }}" name="price" ></textarea>
-		</div>
+			<div class="form-group">
+                <button name="sell" value="sell" type="submit" class="btn btn-primary">Sell this product</button>
+                <button name="preview" value="preview" type="submit" class="btn btn-success">Preview this product</button>
+            </div>
 
-		<div class="form-group">
-			<label for="image">Images</label>
-			<textarea type="body" class="form-control" id="imageLocation"  value="{{ old('imageLocation') }}" placeholder="Just paste a product image url here..." name="imageLocation" ></textarea>
-		</div>
-
-
-		<div class="form-group">
-				<button type="submit" class="btn btn-primary">Sell this product</button>
-		</div>
-
-		@include('includes.errors')
-	</form>
+			@include('includes.errors')
+		</form>
+	</div>
 </div>
 @endsection
